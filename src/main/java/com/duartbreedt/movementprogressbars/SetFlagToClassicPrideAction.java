3049d122e838e043d492bd62fca02419d8f60cb5
Duart <duart.breedt@entelect.co.za>
2021-09-21 20:45:00 +0200

    Added icon

--- src/main/java/com/duartbreedt/movementprogressbars/SetFlagToClassicPrideAction.java
+++ src/main/java/com/duartbreedt/movementprogressbars/SetFlagToClassicPrideAction.java
@@ -9,12 +9,6 @@ public class SetFlagToClassicPrideAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-//        MovementSettingsService service = ApplicationManager.getApplication().getService(MovementSettingsService.class);
-//        MovementSettingsService.State state = service.getState();
-//        if(state != null) {
-//            state.colors = ClassicPride.getColors();
-//        }
-
         PropertiesComponent.getInstance().setValue(FlagColor.PROPERTY_KEY, ClassicPride.class.getName());
     }
 
